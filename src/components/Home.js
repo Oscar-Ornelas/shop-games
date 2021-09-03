08c48157c546d917d27d8781b1208337066bc68f
Oscar Ornelas <oscarornelas98123@gmail.com>
2021-09-03 16:18:40 -0500

    Increase search query page size to 20

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,7 @@ function Home() {
   const API_KEY = process.env.REACT_APP_RAWG_API_KEY;
   return (
     <GameList
-      searchQuery={`https://api.rawg.io/api/games?key=${API_KEY}&dates=2020-10-10,2021-09-01&ordering=-added&page_size=10`}
+      searchQuery={`https://api.rawg.io/api/games?key=${API_KEY}&dates=2020-10-10,2021-09-01&ordering=-added&page_size=20`}
     />
   )
 }
