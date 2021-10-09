5bf800c250b8c3ef17a21e27130a5b7c6f72a6d3
Oscar Ornelas <oscarornelas98123@gmail.com>
2021-10-09 04:44:00 -0500

    gh-pages

---
+++ precache-manifest.b82e2f2db780c6095fbc487211b65b39.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf42624b691d19ff5586",
+    "url": "/landing-page/css/app.c2f5d32f.css"
+  },
+  {
+    "revision": "abc040af87404cb6e233",
+    "url": "/landing-page/css/chunk-vendors.cc250ef3.css"
+  },
+  {
+    "revision": "1d0c2fb13fb0478b4f3a59afdcccb2b3",
+    "url": "/landing-page/img/forest_background.1d0c2fb1.png"
+  },
+  {
+    "revision": "9c2752bae636cc6bfdeed2448fa3a09f",
+    "url": "/landing-page/index.html"
+  },
+  {
+    "revision": "bf42624b691d19ff5586",
+    "url": "/landing-page/js/app.60f2a7a0.js"
+  },
+  {
+    "revision": "abc040af87404cb6e233",
+    "url": "/landing-page/js/chunk-vendors.37000901.js"
+  },
+  {
+    "revision": "4b1ef3de29ba2992b853232d2521304e",
+    "url": "/landing-page/manifest.json"
+  }
+]);
