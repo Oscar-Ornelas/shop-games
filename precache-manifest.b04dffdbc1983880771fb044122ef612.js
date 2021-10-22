f5a6dc06259e445b2a0126a0272463207e210ca4
Oscar Ornelas <oscarornelas98123@gmail.com>
2021-10-22 16:00:55 -0500

    Updates

---
+++ precache-manifest.b04dffdbc1983880771fb044122ef612.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae00d4b03d7b261177e4516fd92fe6fe",
+    "url": "/shop-games/index.html"
+  },
+  {
+    "revision": "7e0590939f34e7552fbf",
+    "url": "/shop-games/static/css/main.aa021fc8.chunk.css"
+  },
+  {
+    "revision": "70f11ff3c88478426fc8",
+    "url": "/shop-games/static/js/2.35d1a458.chunk.js"
+  },
+  {
+    "revision": "a1a636632721a402612df3e043dd630d",
+    "url": "/shop-games/static/js/2.35d1a458.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e0590939f34e7552fbf",
+    "url": "/shop-games/static/js/main.828a7624.chunk.js"
+  },
+  {
+    "revision": "bc27b82e3ae88c49d419",
+    "url": "/shop-games/static/js/runtime-main.6ef84e8e.js"
+  },
+  {
+    "revision": "2a6e84c5db30eb116a8bd2715f7fd1bd",
+    "url": "/shop-games/static/media/rawgapilogo.2a6e84c5.PNG"
+  }
+]);
